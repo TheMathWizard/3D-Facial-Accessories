055ae2f7b3076523f9a67bc7ebdecaf1bf715e10
parthsingh97 <parth.singh97@gmail.com>
2019-04-10 14:55:26 +0530

    Add files via upload
    Blender_Path

--- master.py
+++ master.py
@@ -1,9 +1,9 @@
 import subprocess
-
-subprocess.run(["/Applications/Blender/blender.app/Contents/MacOS/blender", "--background", "--python", "adjust_backhead.py"])
-subprocess.run(["/Applications/Blender/blender.app/Contents/MacOS/blender", "--background","--python", "extract_border_backhead.py"])
+Blender_Path = "/Applications/Blender/blender.app/Contents/MacOS/blender"
+subprocess.run([Blender_Path, "--background", "--python", "adjust_backhead.py"])
+subprocess.run([Blender_Path, "--background","--python", "extract_border_backhead.py"])
 subprocess.run(["python3", "meshing.py"])
-subprocess.run(["/Applications/Blender/blender.app/Contents/MacOS/blender", "--background","--python", "stitched_with_head.py"])
+subprocess.run([Blender_Path, "--background","--python", "stitched_with_head.py"])
 subprocess.run(["python3", "color_full_model.py"])
 
 #/Applications/Blender/blender.app/Contents/MacOS/blender --python adjust_backhead.py
